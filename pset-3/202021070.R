2ccad969d4dd73ca396b69a1caa55f5d6df31e80
fandino-julian <j.fandinoa@uniandes.edu.co>
2024-04-22 08:56:36 -0500

    Cargar librerias

---
+++ pset-3/202021070.R
@@ -0,0 +1,11 @@
+##Problem Set 3
+## Julian Fandiño_202021070##
+version
+## version.string R version 4.3.2 (2023-10-31)##
+
+# Cargar las librerías
+library(pacman)
+library(rio)
+library(data.table)
+library(tidyverse)
+
